cd00d79fc9226b631fb0eb902b18b37185a76b94
Deepesh Gupta <guptad28031998@gmail.com>
2023-08-14 09:33:51 +0530

    A1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-  
+    public static void main(String agrs[]){
+        RWOnly r = new RWOnly("name");
+        System.out.println(r.getName());
+        r.setName("name");
+        System.out.println(r.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+
+    public RWOnly(){
+
+    }
+    public RWOnly(String nmae) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
